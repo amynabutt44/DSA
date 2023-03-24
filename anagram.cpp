89be0bea6eaf920642a5da928a5b73aa05ade04b
amynabutt44 <116289541+amynabutt44@users.noreply.github.com>
2023-03-24 14:29:32 +0500

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -1,56 +1,23 @@
+
 #include<iostream>
 using namespace std;
-// so this program works as folow
-// we conider a sorted array vo checking wether there exsist two numbers which sum ios equal
-// the target or not 
-bool checkingtarget(int* arr, int size, int target)
+bool checkingpalindrome(string n)
 {
 	int left = 0;
-	int right = size - 1;
+	int right = n.length() - 1;
 	while (left < right)
 	{
-		int a = arr[left] + arr[right];
-		if (a == target)
-			return true;
-		else if (a > target)
-			right--;
-		else
-			left--;
+		if (n[left] != n[right])
+			return false;
+		left++;
+		right--;
 	}
-	return false;
+	return true;
 }
 int main()
 {
-	int* a;
-	a = new int[5];
-	for (int i = 0; i < 5; i++)
-	{
-		a[i] = i + 2;
-	}
-	if (checkingtarget(a, 5, 8))
-		cout << (" ohh yeah target exsist ");
-	else
-		cout << (" :( no target not exsist");
+	if(checkingpalindrome("amnma"))
+		cout<<(" yeah its palindrome");
+
+		
 }
-//#include<iostream>
-//using namespace std;
-//bool checkingpalindrome(string n)
-//{
-//	int left = 0;
-//	int right = n.length() - 1;
-//	while (left < right)
-//	{
-//		if (n[left] != n[right])
-//			return false;
-//		left++;
-//		right--;
-//	}
-//	return true;
-//}
-//int main()
-//{
-//	if(checkingpalindrome("amnma"))
-//		cout<<(" yeah its palindrome");
-//
-//		
-//}
