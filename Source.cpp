de0f0c4c6037fd965eb2c98fdc7a605118f3a88c
amynabutt44 <116289541+amynabutt44@users.noreply.github.com>
2023-04-07 19:12:10 +0500

    linear single link list

--- Source.cpp
+++ Source.cpp
@@ -1,171 +1,194 @@
 #include <iostream>
-#include <initializer_list>
 using namespace std;
-template<typename T>
-class NDArray
+
+class node
 {
-	T* data;
-	int  noofdimensions;
-	int* sizeofdimensions;
-	int* startingindexofdimensions;
-	int rowmapping(const initializer_list <int>& a)
-	{
-		int number = 0;
-		int index = 0;
-		int sum = 0;
-		for (auto b : a)
-		{
-			number= b-startingindexofdimensions[index]
-				for (int i = index + 1; i < noofdimensions; i++)
-				{
-					number *= sizeofdimensions[i];
-			    }
-			sum += number;
-		}
-		return sum;
-	}
-
 public:
-	NDArray(const initializer_list<int> list = {}, const initializer_list<int> list2 = {})
+	int data;
+	node* next;
+	node(int val)
 	{
-		noofdimensions = list.size();
-		if (noofdimensions == 0)
-		{
-			sizeofdimensions = nullptr;
-			data = nullptr;
+		data = val;
+		next = nullptr;
+	}
 
-		}
-		else
-		{
-			sizeofdimensions = new int[noofdimensions];
-			startingindexofdimensions = new int[noofdimensions];
-			int i = 0;
-			for (auto a : list)
-			{
-				sizeofdimensions[i] = a;
-				i++;
-			}
-			
-		}
-		int size = 1;
-		for (int j = 0; j < noofdimensions; j++)
-		{
-			size *= noofdimensions[j];
-		}
-		data = new T[size];
-		if (list2.size() == 0)
-		{
-			
-			for (int i = 0; i < noofdimensions; i++)
-			{
-				startingindexofdimensions[i] = 0;
-			}
-		}
-	
-		else
-		{
-			int j = 0;
-			for (auto a : list2)
-			{
-				startingindexofdimensions[i] = a;
-				j++;
-			}
-		}
-	}
-	NDArray(const NDArray<T>& a)
-	{
-		if (a.data = nullptr)
-		{
-			this->~NDArray();
-		}
-		noofdimensions = a.noofdimensions;
-		startingindexofdimensions = new int[noofdimensions];
-		sizeofdimensions = new int[noofdimensions];
-		int size = 1;
-		for (int i = 0; i < a.noofdimensions; i++)
-		{
-			size *= a.noofdimensions[i];
-		}
-		data = new T[size];
-		for (int i = 0; i < noofdimensions; i++)
-		{
-			sizeofdimensions[i] = a.sizeofdimensions[i];
-			noofdimensions[i] = a.noofdimensions[i];
-			data[i] = a.data[i];
-		}
-	}
-	NDArray<T>& operator=(const NDArray <T>& a)
-	{
-		if (a.data = nullptr)
-		{
-			this->~NDArray();
-		}
-		if (this == &a)
-		{
-			return *this;
-		}
-		noofdimensions = a.noofdimensions;
-		startingindexofdimensions = new int[noofdimensions];
-		sizeofdimensions = new int[noofdimensions];
-		int size = 1;
-		for (int i = 0; i < noofdimensions; i++)
-		{
-			size *= noofdimensions[i];
-		}
-		data = new T[size];
-		for (int i = 0; i < noofdimensions; i++)
-		{
-			sizeofdimensions[i] = a.sizeofdimensions[i];
-			noofdimensions[i] = a.noofdimensions[i];
-			data[i] = a.data[i];
-		}
-	}
-	int getnumberofdimension()
-	{
-		return noofdimensions;
-	}
-	int getsizeofdimension(int i)
-	{
-		return sizeofdimensions[i];
-	}
-	int getdimensionlowerindex(int i)
-	{
-		return startingindexofdimensions[i];
-	}
-	int gethigherindexdimension(int i)
-	{
-		return startingindexofdimensions[i] + sizeofdimensions[i] - 1;
-	}
-	bool isvalidindex(initializer_list <int> &list)
-	{
-		int i = 0;
-		for (auto a : list)
-		{
-			if (!(a >= getdimensionlowerindex[i] && a <= getdimensionlowerindex[i]))
-			{
-				return false;
-			}
-			return true;
-		}
-	}
-	~NDArray()
-	{
-		delete[]data;
-		delete[]noofdimensions;
-		delete[]startingindexofdimensions;
-		data = nullptr;
-		noofdimensions = nullptr;
-		startingindexofdimensions = nullptr;
-		noofdimensions = 0;
-	}
-	T& operator()(const initializer_list<int>& a)
-	{
-		int index = rowmapping(a);
-		return data[i];
-	}
 };
 
+class LSLL
+{
+	node* head;
+public:
+	LSLL()
+	{
+		head = nullptr;
+	}
+	void insertattail(int val)
+	{
+		node* ptr = new node(val);
+		if (head == nullptr)
+		{
+			head = ptr;
+			return;
+		}
+		node* x = head;
+		while (x->next != nullptr)
+		{
+			x = x->next;
+		}
+		x->next = ptr;
+	}
+	void insertathead(int val)
+	{
+		node* ptr = new node(val);
+		if (head == nullptr)
+		{
+			head = ptr;
+			return;
+		}
+		ptr->next = head;
+		head = ptr;
+	}
+	void insertafter(int val, int key)
+	{
+		
+		if (!head)
+		{
+			return;
+		}
+		node* ptr = new node(val);
+		node* x = head;
+		while (x->next!=nullptr&& x->data!=key)
+		{
+			x = x->next;
+		}
+		if (!x)
+		{
+			return;
+		}
+		ptr->next = x->next;
+		x->next = ptr;
+	}
+	void insertbeofore( int val,int key)
+	{
+		if (!head)
+		{
+			return;
+		}
+		node* ptr = new node(val);
+		if (head->data == key)
+		{
+			ptr->next = head;
+			head = ptr;
+			return;
+
+		}
+		
+		node* prev = head;
+		node* ne = prev->next;
+		while (ne != nullptr && ne->data != key)
+		{
+			prev = ne;
+			ne = ne->next;
+		}
+		if (!ne) {
+			return;
+		}
+		ptr->next = prev->next;
+		prev->next = ptr;
+
+	}
+	void removeathead()
+	{
+		if (!head)
+		{
+			return;
+		}
+		node* x = head;
+		head = head->next;
+		delete x;
+	}
+	void removeattail()
+	{
+		if (!head)
+			return;
+		if (head->next == nullptr)
+		{
+			delete head;
+			head = nullptr;
+		}
+		node* ptr = head;
+		node * current = head->next;
+		while (current != nullptr)
+		{          
+			ptr = current;
+			current = current->next;
+		}
+		ptr->next = nullptr;
+		delete current;
+	}
+	void removeafter(int key)
+	{
+		if (!head)
+			return;
+		node* ptr = head;
+		node* x = nullptr;
+		while (ptr != nullptr && ptr->data != key)
+		{
+			ptr = ptr->next;
+		}
+		if (ptr != nullptr)
+		{
+			x = ptr->next;
+			ptr->next = x->next;
+			delete x;
+		}
+	}
+	void removebefore(int val)
+	{
+		if (!head)
+			return;
+		if (head->next->data == val)
+		{
+			removeathead();
+			return;
+		}
+		node* ptr = head;
+		while (ptr->next->next != nullptr && ptr->next->next->data != val)
+		{
+			ptr = ptr->next;
+		}
+		node* x = ptr->next;
+		if (ptr->next->next != nullptr)
+		{
+			ptr->next = ptr->next->next;
+			delete x;
+		}
+	}
+	void print()
+	{
+		node* ptr = head;
+		while (ptr)
+		{
+			cout << ptr->data<<endl;
+			ptr = ptr->next;
+		}
+	}
+
+};
 int main()
 {
-
+	LSLL  one;
+	one.insertattail(7);
+	one.insertathead(4);
+	one.insertafter(6,4);
+	one.removeafter(3);
+	one.insertafter(3,7);
+	one.insertathead(2);
+	one.insertbeofore(1,3);
+	one.insertathead(23);
+	one.insertafter(12, 4);
+	one.removeafter(4);
+	one.removeathead();
+	one.removebefore(7);
+	one.print();
 }
