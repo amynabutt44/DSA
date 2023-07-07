0ae3ced2d71da504f54dba3689eff8b4d6dd8f96
amynabutt44 <116289541+amynabutt44@users.noreply.github.com>
2023-07-07 19:01:20 +0500

    decimal to binary

--- Source.cpp
+++ Source.cpp
@@ -1,232 +1,54 @@
 #include <iostream>
+#include<math.h>
 using namespace std;
-
-class node
+bool binarysearch(int * ptr, int start, int end,int key)
 {
-public:
-	int data;
-	node* next;
-	node(int val)
+	
+	while (start <= end)
 	{
-		data = val;
-		next = nullptr;
+		int mid = (start + end) / 2;
+		if (ptr[mid] == key)
+			return true;
+		else if (ptr[mid] > key)
+		{
+			end = mid - 1;
+		}
+		else
+			start = mid + 1;
 	}
-
-};
-
-class LSLL
-{
-	node* head;
-public:
-	class Iterator
-	{
-		friend class LSLL; 
-		node* pointee;
-		Iterator(node* curr) : pointee(curr)
-		{}
-	public:
-		int& operator * ()
-		{
-			if(pointee)
-			return pointee->data;
-			exit(0);
-		}
-		const Iterator& operator ++()
-		{
-			if (pointee == nullptr)
-				exit(0);
-			pointee = pointee->next;
-			return *this;
-		}
-		bool operator != (const Iterator& other)const
-		{
-			return pointee != other.pointee;
-
-		}
-
-	};
-	LSLL()
-	 {
-		head = nullptr;
-	}
-	void insertattail(int val)
-	{
-		node* ptr = new node(val);
-		if (head == nullptr)
-		{
-			head = ptr;
-			return;
-		}
-		node* x = head;
-		while (x->next != nullptr)
-		{
-			x = x->next;
-		}
-		x->next = ptr;
-	}
-	void insertathead(int val)
-	{
-		node* ptr = new node(val);
-		if (head == nullptr)
-		{
-			head = ptr;
-			return;
-		}
-		ptr->next = head;
-		head = ptr;
-	}
-	void insertafter(int val, int key)
-	{
-		
-		if (!head)
-		{
-			return;
-		}
-		node* ptr = new node(val);
-		node* x = head;
-		while (x->next!=nullptr&& x->data!=key)
-		{
-			x = x->next;
-		}
-		if (!x)
-		{
-			return;
-		}
-		ptr->next = x->next;
-		x->next = ptr;
-	}
-	void insertbeofore( int val,int key)
-	{
-		if (!head)
-		{
-			return;
-		}
-		node* ptr = new node(val);
-		if (head->data == key)
-		{
-			ptr->next = head;
-			head = ptr;
-			return;
-
-		}
-		
-		node* prev = head;
-		node* ne = prev->next;
-		while (ne != nullptr && ne->data != key)
-		{
-			prev = ne;
-			ne = ne->next;
-		}
-		if (!ne) {
-			return;
-		}
-		ptr->next = prev->next;
-		prev->next = ptr;
-
-	}
-	void removeathead()
-	{
-		if (!head)
-		{
-			return;
-		}
-		node* x = head;
-		head = head->next;
-		delete x;
-	}
-	void removeattail()
-	{
-		if (!head)
-			return;
-		if (head->next == nullptr)
-		{
-			delete head;
-			head = nullptr;
-		}
-		node* ptr = head;
-		node * current = head->next;
-		while (current != nullptr)
-		{          
-			ptr = current;
-			current = current->next;
-		}
-		ptr->next = nullptr;
-		delete current;
-	}
-	void removeafter(int key)
-	{
-		if (!head)
-			return;
-		node* ptr = head;
-		node* x = nullptr;
-		while (ptr != nullptr && ptr->data != key)
-		{
-			ptr = ptr->next;
-		}
-		if (ptr != nullptr)
-		{
-			x = ptr->next;
-			ptr->next = x->next;
-			delete x;
-		}
-	}
-	void removebefore(int val)
-	{
-		if (!head)
-			return;
-		if (head->next->data == val)
-		{
-			removeathead();
-			return;
-		}
-		node* ptr = head;
-		while (ptr->next->next != nullptr && ptr->next->next->data != val)
-		{
-			ptr = ptr->next;
-		}
-		node* x = ptr->next;
-		if (ptr->next->next != nullptr)
-		{
-			ptr->next = ptr->next->next;
-			delete x;
-		}
-	}
-	void print()
-	{
-		node* ptr = head;
-		while (ptr)
-		{
-			cout << ptr->data<<endl;
-			ptr = ptr->next;
-		}
-	}
-	Iterator begin()
-	{
-		return Iterator(head);
-	}
-};
+	return false;
+}
 int main()
 {
-	LSLL  one;
+	/*int n;
+	int ans = 0;
+	int i = 0;
+	cout << " enter  a number " << endl;
+	cin >> n;
+	while (n != 0)
+	{
+		int digit = n % 10;
+		n = n / 10;
+		if (digit == 1)
+		{
+			ans = ans + pow(2, i);
+		}
+		i++;
+	}
+	cout << ans;*/
+	//int n;
+	//int ans=0;
+	//cout << " enter a number" << endl;
+	//cin >> n;
+	//for (int i = 0; n > 0; i++)
+	//{
+	//	int rem = n % 2;
+	//	n = n / 2;
+	//	ans = (rem * pow(10, i)) + ans;
 
-
-	one.insertattail(7);
-	one.insertattail(2);
-	one.insertattail(13);
-	LSLL::Iterator a = one.begin();
-	++a;
-	cout << " aaaa " << endl;
-	cout << *a;
-	/*one.insertathead(4);
-	one.insertafter(6,4);
-	one.removeafter(3);
-	one.insertafter(3,7);
-	one.insertathead(2);
-	one.insertbeofore(1,3);
-	one.insertathead(23);
-	one.insertafter(12, 4);
-	one.removeafter(4);
-	one.removeathead();
-	one.removebefore(7);*/
-	
+	//}
+	///*int digit = n & 1;
+	//cout << digit;
+	//ans =( digit * pow(10, 0));*/
+	//cout << ans;
 }
