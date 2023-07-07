ca3c4d4900785feacc92e79e7b80486cebb815ae
amynabutt44 <116289541+amynabutt44@users.noreply.github.com>
2023-07-07 19:08:45 +0500

    middle elemnt of the linklist

--- Source.cpp
+++ Source.cpp
@@ -1,54 +1,204 @@
 #include <iostream>
-#include<math.h>
 using namespace std;
-bool binarysearch(int * ptr, int start, int end,int key)
+
+class node
 {
-	
-	while (start <= end)
+public:
+	int data;
+	node* next;
+	node(int val)
 	{
-		int mid = (start + end) / 2;
-		if (ptr[mid] == key)
-			return true;
-		else if (ptr[mid] > key)
-		{
-			end = mid - 1;
-		}
-		else
-			start = mid + 1;
+		data = val;
+		next = nullptr;
 	}
-	return false;
-}
+
+};
+
+class LSLL
+{
+	node* head;
+public:
+	LSLL()
+	{
+		head = nullptr;
+	}
+	void insertattail(int val)
+	{
+		node* ptr = new node(val);
+		if (head == nullptr)
+		{
+			head = ptr;
+			return;
+		}
+		node* x = head;
+		while (x->next != nullptr)
+		{
+			x = x->next;
+		}
+		x->next = ptr;
+	}
+	void insertathead(int val)
+	{
+		node* ptr = new node(val);
+		if (head == nullptr)
+		{
+			head = ptr;
+			return;
+		}
+		ptr->next = head;
+		head = ptr;
+	}
+	void insertafter(int val, int key)
+	{
+
+		if (!head)
+		{
+			return;
+		}
+		node* ptr = new node(val);
+		node* x = head;
+		while (x->next != nullptr && x->data != key)
+		{
+			x = x->next;
+		}
+		if (!x)
+		{
+			return;
+		}
+		ptr->next = x->next;
+		x->next = ptr;
+	}
+	void insertbeofore(int val, int key)
+	{
+		if (!head)
+		{
+			return;
+		}
+		node* ptr = new node(val);
+		if (head->data == key)
+		{
+			ptr->next = head;
+			head = ptr;
+			return;
+
+		}
+
+		node* prev = head;
+		node* ne = prev->next;
+		while (ne != nullptr && ne->data != key)
+		{
+			prev = ne;
+			ne = ne->next;
+		}
+		if (!ne) {
+			return;
+		}
+		ptr->next = prev->next;
+		prev->next = ptr;
+
+	}
+	void removeathead()
+	{
+		if (!head)
+		{
+			return;
+		}
+		node* x = head;
+		head = head->next;
+		delete x;
+	}
+	void removeattail()
+	{
+		if (!head)
+			return;
+		if (head->next == nullptr)
+		{
+			delete head;
+			head = nullptr;
+		}
+		node* ptr = head;
+		node* current = head->next;
+		while (current != nullptr)
+		{
+			ptr = current;
+			current = current->next;
+		}
+		ptr->next = nullptr;
+		delete current;
+	}
+	void removeafter(int key)
+	{
+		if (!head)
+			return;
+		node* ptr = head;
+		node* x = nullptr;
+		while (ptr != nullptr && ptr->data != key)
+		{
+			ptr = ptr->next;
+		}
+		if (ptr != nullptr)
+		{
+			x = ptr->next;
+			ptr->next = x->next;
+			delete x;
+		}
+	}
+	void removebefore(int val)
+	{
+		if (!head)
+			return;
+		if (head->next->data == val)
+		{
+			removeathead();
+			return;
+		}
+		node* ptr = head;
+		while (ptr->next->next != nullptr && ptr->next->next->data != val)
+		{
+			ptr = ptr->next;
+		}
+		node* x = ptr->next;
+		if (ptr->next->next != nullptr)
+		{
+			ptr->next = ptr->next->next;
+			delete x;
+		}
+	}
+	void print()
+	{
+		node* ptr = head;
+		while (ptr)
+		{
+			cout << ptr->data << endl;
+			ptr = ptr->next;
+		}
+	}
+	node* middlenode()
+	{
+		node* slow = head;
+		node* fast = head;
+		while (fast&&fast!=nullptr)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		return slow;
+
+	}
+};
 int main()
 {
-	/*int n;
-	int ans = 0;
-	int i = 0;
-	cout << " enter  a number " << endl;
-	cin >> n;
-	while (n != 0)
+	LSLL  one;
+	one.insertattail(7);
+	one.insertathead(5);
+	one.insertathead(3);
+	one.insertathead(2);
+	/*one.insertathead(1);*/
+	one.print();
+	node * ptr=one.middlenode();
+	while (ptr)
 	{
-		int digit = n % 10;
-		n = n / 10;
-		if (digit == 1)
-		{
-			ans = ans + pow(2, i);
-		}
-		i++;
+		cout << ptr->data;
+		ptr = ptr->next;
 	}
-	cout << ans;*/
-	//int n;
-	//int ans=0;
-	//cout << " enter a number" << endl;
-	//cin >> n;
-	//for (int i = 0; n > 0; i++)
-	//{
-	//	int rem = n % 2;
-	//	n = n / 2;
-	//	ans = (rem * pow(10, i)) + ans;
-
-	//}
-	///*int digit = n & 1;
-	//cout << digit;
-	//ans =( digit * pow(10, 0));*/
-	//cout << ans;
 }
