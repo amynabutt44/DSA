fc9a5815bacbf32cdbbf7253c56692a765794a60
amynabutt44 <amnashahzadi740@gmail.com>
2025-01-06 21:08:09 +0500

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+
+namespace WebApplication3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
